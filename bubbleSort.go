e7bee8af8805c815943c1dfcf11f9c1980718c77
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2018-03-11 20:21:07 -0400

    Bubble sort.

---
+++ bubbleSort.go
@@ -0,0 +1,23 @@
+package main
+
+/*
++--------------------------+---------------------+
+|    Time complexity       |   Space Complexity  |
++------+----------+--------+---------------------+
+| Best |  Average | Worst  |        Worst        |
++------+----------+--------+---------------------+
+| Ω(n) |  Θ(n^2)  | O(n^2) |        O(1)         |
++------+----------+--------+---------------------+
+*/
+
+func bubblesort(array []int) {
+	for i := 0; i < len(array); i++ {
+		for j := len(array) - 1; j > i; j-- {
+			if array[j-1] > array[j] {
+				x := array[j-1]
+				array[j-1] = array[j]
+				array[j] = x
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	array := []int{2, 7, 1, 9, 5, 4, 3, 3}
 	fmt.Println("Unsorted array:", array)
-	insertionSort(array)
+	bubblesort(array)
 	fmt.Println("Sorted array:", array)
 }
