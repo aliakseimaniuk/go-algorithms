262d42dcc7b9e1b152d7cb70f201426f3703d41e
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2018-03-30 20:09:26 -0400

    Simplified code for bubble sort.

--- bubbleSort.go
+++ bubbleSort.go
@@ -14,9 +14,7 @@ func bubblesort(array []int) {
 	for i := 0; i < len(array); i++ {
 		for j := len(array) - 1; j > i; j-- {
 			if array[j-1] > array[j] {
-				x := array[j-1]
-				array[j-1] = array[j]
-				array[j] = x
+				array[j-1], array[j] = array[j], array[j-1]
 			}
 		}
 	}
